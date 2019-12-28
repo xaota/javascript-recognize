637c841827dbd96684624105842b3b7b99ec14f0
xaota <1172619+xaota@users.noreply.github.com>
2019-12-28 17:09:57 +0500

    Переключение отображения оригинала

--- library/client/components/x-sequence/x-sequence.js
+++ library/client/components/x-sequence/x-sequence.js
@@ -1,5 +1,6 @@
 import Component           from '/material/script/Component.js';
 import MaterialChipTooltip from '/material/components/chip-tooltip/material-chip-tooltip.js';
+import MaterialSwitch      from '/material/components/switch/material-switch.js';
 
 import Entity from '../../../model/Entity.js';
 
@@ -11,30 +12,52 @@ const component = Component.meta(import.meta.url, 'x-sequence');
   /**
     *
     */
-    constructor(sequence = []) {
+    constructor(sequence = [], origin = '') {
       super(component);
-      this.store({sequence});
+      this.store({sequence, origin});
     }
 
   /** */
     mount(node) {
-      const {sequence, positive = []} = this.store();
+      const switcher = node.querySelector('material-switch');
 
-      this.innerHTML = '';
-      sequence.forEach(token => {
-        const tag = token.entity
-          ? createChipNode(token, positive)
-          : createSpanNode(token);
-        this.appendChild(tag);
+      switcher.addEventListener('change', e => {
+        const {sequence = [], origin = '', positive = []} = this.store();
+        switcher.checked
+          ? showSequence(this, sequence, positive)
+          : showOriginText(this, origin);
       });
 
       return this;
+    }
+
+  /** */
+    init() {
+      // const node = this.shadowRoot;
+      const {sequence, origin, positive = []} = this.store();
+      showSequence(this, sequence, positive);
     }
   }
 
 Component.define(component, XSequence);
 
 // #region [Private]
+/** показывает результаты разбора (список токенов) / showSequence */
+  function showSequence(node, sequence, positive) {
+    node.innerHTML = '';
+    sequence.forEach(token => {
+      const tag = token.entity
+        ? createChipNode(token, positive)
+        : createSpanNode(token);
+      node.appendChild(tag);
+    });
+  }
+
+/** показывает оригинальное сообщение / showOriginText */
+  function showOriginText(node, text) {
+    node.innerHTML = text;
+  }
+
 /** / createChipNode */
   function createChipNode(token, positive = []) {
     const tag = new MaterialChipTooltip(token.match, '@' + token.entity.name); // Entity.from(token.entity).toString()
